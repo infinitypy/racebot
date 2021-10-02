a506b5e4ef995ec35ebc49897781bc0c1f0139cb
Randy <randy5yudi2zhang4@gmail.com>
2021-10-01 20:17:40 -0700

    stuff

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import discord
 from discord.ext import commands
+import sheets
 
 client = commands.Bot(command_prefix='~')
+
 
 @client.event
 async def on_ready():
@@ -12,6 +13,10 @@ async def on_ready():
 async def hello(ctx):
     await ctx.send('hello')
 
+
+@client.command()
+async def race(ctx, num):
+    await ctx.send(sheets.get_race_name(num))
 
 
 client.run('ODkzMjkxMjI1NTY4OTE5NTYy.YVZUSA.qCUMnKzLJsIu0X_TCEI5BnvST8k')
--- sheets.py
+++ sheets.py
@@ -1,19 +1,14 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
-
+scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
-
 client = gspread.authorize(creds)
-
 sheet = client.open_by_key("1iG4mtopJVlF7g3gv6sNrYspyonSQGSHuDOb-QMNVnPc")
-
 worksheet = sheet.worksheet("raceinfo")
 
 
-
-row = int(input())
-col = 2
-cell = worksheet.cell(row+1,col).value
-print(cell)
+def get_race_name(num):
+    col = 2
+    return worksheet.cell(num + 1, col).value
