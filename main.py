e4ac3b0d6d71c00e534c439ed86471b42d0d1273
Randy <randy5yudi2zhang4@gmail.com>
2021-10-01 21:29:47 -0700

    length, rtime

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import datetime
 import sheets
 
 client = commands.Bot(command_prefix='~')
@@ -17,6 +18,18 @@ async def hello(ctx):
 @client.command()
 async def race(ctx, num):
     await ctx.send(sheets.get_race_name(num))
+
+
+@client.command()
+async def length(ctx, num, abr=None):
+    await ctx.send('Round ' + num + ' is ' + sheets.length(int(num), abr) + 's')
+
+
+@client.command()
+async def rtime(ctx, start, end, stime, abr=None):
+    longest, longest_round = sheets.last_bloon_time(int(start), int(end), float(stime), abr)
+    final_time = str(datetime.timedelta(seconds=longest))[3:-4]
+    await ctx.send('You will get **' + final_time + '** if you perfect clean round ' + str(longest_round))
 
 
 client.run('ODkzMjkxMjI1NTY4OTE5NTYy.YVZUSA.qCUMnKzLJsIu0X_TCEI5BnvST8k')
--- sheets.py
+++ sheets.py
@@ -1,14 +1,32 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from decimal import Decimal
 
 scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
 client = gspread.authorize(creds)
 sheet = client.open_by_key("1iG4mtopJVlF7g3gv6sNrYspyonSQGSHuDOb-QMNVnPc")
-worksheet = sheet.worksheet("raceinfo")
+race_info = sheet.worksheet("raceinfo")
+round_info = sheet.worksheet("rounds")
 
 
 def get_race_name(num):
     col = 2
-    return worksheet.cell(num + 1, col).value
+    return race_info.cell(num + 1, col).value
+
+
+def length(num, abr):
+    col = 2 if abr is not None else 1
+    return round_info.cell(num + 1, col).value
+
+
+def last_bloon_time(start, end, stime, abr):
+    col = 2 if abr is not None else 1
+    rounds = round_info.range(start + 1, col, end + 1, col)
+    rounds_adj = []
+    for i, r in enumerate(rounds):
+        rounds_adj.append(float(r.value) + (i * 0.2))
+    longest = max(rounds_adj)
+    longest_round = rounds_adj.index(longest)
+    return round(longest + stime, 2), longest_round + start
