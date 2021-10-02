96625b6b7f991a1bf94ce71ef5b2e70ca77c0978
infinity-boop <71298040+infinity-boop@users.noreply.github.com>
2021-10-02 15:40:51 -0400

    fixed rtime, added id to race command, hosting

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import os
 from discord.ext import commands
 import datetime
 import sheets
+from webserver import keep_alive
+
 
 client = commands.Bot(command_prefix='~')
 
@@ -14,16 +17,18 @@ async def on_ready():
 async def hello(ctx):
     await ctx.send('hello')
 
+@client.command()
+async def invite(ctx):
+    await ctx.send('https://discord.com/oauth2/authorize?client_id=893291225568919562&permissions=3072&scope=bot')
 
 @client.command()
-async def race(ctx, num):
-    await ctx.send(sheets.get_race_name(num))
+async def race(ctx, num, id=None):
+    await ctx.send(sheets.get_race_name(int(num), id))
 
 
 @client.command()
 async def length(ctx, num, abr=None):
     await ctx.send('Round ' + num + ' is ' + sheets.length(int(num), abr) + 's')
-
 
 @client.command()
 async def rtime(ctx, start, end, stime, abr=None):
@@ -31,5 +36,7 @@ async def rtime(ctx, start, end, stime, abr=None):
     final_time = str(datetime.timedelta(seconds=longest))[3:-4]
     await ctx.send('You will get **' + final_time + '** if you perfect clean round ' + str(longest_round))
 
+keep_alive()
+my_secret = os.environ['TOKEN']
+client.run(my_secret)
 
-client.run('ODkzMjkxMjI1NTY4OTE5NTYy.YVZUSA.qCUMnKzLJsIu0X_TCEI5BnvST8k')
--- sheets.py
+++ sheets.py
@@ -11,9 +11,12 @@ race_info = sheet.worksheet("raceinfo")
 round_info = sheet.worksheet("rounds")
 
 
-def get_race_name(num):
-    col = 2
-    return race_info.cell(num + 1, col).value
+def get_race_name(num, id):
+    col = 3 if id is not None else 2
+    if num == 129:
+        return ':corn::tada:'
+    else:
+        return race_info.cell(num + 1, col).value
 
 
 def length(num, abr):
@@ -23,10 +26,15 @@ def length(num, abr):
 
 def last_bloon_time(start, end, stime, abr):
     col = 2 if abr is not None else 1
+    bonusdelay = 0
+    if start == 0:
+        start = 1
+        bonusdelay = 0.2
+    
     rounds = round_info.range(start + 1, col, end + 1, col)
     rounds_adj = []
     for i, r in enumerate(rounds):
         rounds_adj.append(float(r.value) + (i * 0.2))
     longest = max(rounds_adj)
     longest_round = rounds_adj.index(longest)
-    return round(longest + stime, 2), longest_round + start
+    return round(longest + stime + bonusdelay + 0.0167 - 0.2 , 2), longest_round + start
