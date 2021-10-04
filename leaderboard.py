fee91a35681a4be35d64c0a88a08593ba4aa22b8
Randy <randy5yudi2zhang4@gmail.com>
2021-10-03 17:35:03 -0700

    leaderboard api fix

--- leaderboard.py
+++ leaderboard.py
@@ -7,20 +7,6 @@ raceurl = 'https://priority-static-api.nkstatic.com/storage/static/appdocs/11/le
 
 data = requests.get(raceurl).json()
 
-d = json.loads(data["data"])
-raw = []
-times = []
-names = []
-scoreoffset = []
-nameoffset = []
-
-for i in range(99):
-    raw.append(str(d["scores"]["equal"][i]))
-for i in range(99):
-    scoreoffset.append(int(raw[i].find('score')))
-    nameoffset.append(raw[i].find('metadata'))
-
-
-for i in range(99):
-    times.append(raw[i][scoreoffset[i]+8:scoreoffset[i]+17])
-print(times[49])
+entries = json.loads(data["data"])['scores']['equal']
+stuff = [(entry['metadata'].split(',')[0], entry['score'], entry['userID']) for entry in entries]
+print(stuff)
