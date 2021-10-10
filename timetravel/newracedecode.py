cd43d5157edb8843e3b1a06cc190a8829fc2fbe5
infinity-boop <71298040+infinity-boop@users.noreply.github.com>
2021-10-10 00:50:38 -0400

    decoding thing works now so we can automate race data

--- timetravel/newracedecode.py
+++ timetravel/newracedecode.py
@@ -1,13 +1,11 @@
 import requests
 
-
-def decode(stuff):
-    for i in range(14, len(stuff)):
-        stuff[i] = chr(ord(stuff[i]) - 21)
-        stuff[i] = chr(ord(stuff[i]) - ((i - 14) % 6))
-    return stuff[14:]
-
-
+def decode(data):
+    data = list(data)
+    for i in range(len(data)):
+        data[i] = data[i] -21
+        data[i] = chr(data[i] - ((i - 14) % 6))
+    return ''.join(data)
+    
 data = requests.get('https://priority-static-api.nkstatic.com/storage/static/multi?appid=11&files=races/Exponential_4')
-decoded = ''.join(decode(list(data.text)))
-print(decoded)
+print(decode(data.content))
