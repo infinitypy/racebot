19ece79d0409c24b692ccf4cfa5d63eb2e6aaa28
infinity-boop <71298040+infinity-boop@users.noreply.github.com>
2021-10-10 00:51:37 -0400

    small fix

--- timetravel/newracedecode.py
+++ timetravel/newracedecode.py
@@ -5,7 +5,7 @@ def decode(data):
     for i in range(len(data)):
         data[i] = data[i] -21
         data[i] = chr(data[i] - ((i - 14) % 6))
-    return ''.join(data)
+    return ''.join(data[14:])
     
 data = requests.get('https://priority-static-api.nkstatic.com/storage/static/multi?appid=11&files=races/Exponential_4')
 print(decode(data.content))
